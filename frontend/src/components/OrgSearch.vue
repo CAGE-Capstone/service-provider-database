a754aefe256398296322141334a0b9db7899bcad
Gabrielle Edwards <175048781+gabredw@users.noreply.github.com>
2026-01-12 11:49:42 -0800

    Create a searchbar
    No functionality yet

---
+++ frontend/src/components/OrgSearch.vue
@@ -0,0 +1,15 @@
+<template>
+
+<!-- search bar -->
+<div>
+      <input
+        v-model="searchQuery"
+        @keyup.enter="searchKeyword(searchQuery)"
+        placeholder="Search by keyword"
+      />
+      <button @click="searchKeyword(searchQuery)"">
+        Search
+      </button>
+</div>
+
+</template>
