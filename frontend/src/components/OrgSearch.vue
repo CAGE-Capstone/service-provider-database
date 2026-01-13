57cc9afe7411fb79656dcf7afe9bd3f1c9a6bf9a
Gabrielle Edwards <175048781+gabredw@users.noreply.github.com>
2026-01-13 09:50:17 -0800

    Implement category buttons
    The buttons draw directly from the backend's list of categories

--- frontend/src/components/OrgSearch.vue
+++ frontend/src/components/OrgSearch.vue
@@ -1,15 +1,49 @@
 <template>
 
-<!-- search bar -->
-<div>
-      <input
-        v-model="searchQuery"
-        @keyup.enter="searchKeyword(searchQuery)"
-        placeholder="Search by keyword"
-      />
-      <button @click="searchKeyword(searchQuery)"">
-        Search
-      </button>
-</div>
+    <!-- search bar -->
+    <div>
+        <input
+            v-model="searchQuery"
+            @keyup.enter="searchKeyword(searchQuery)"
+            placeholder="Search by keyword"
+        />
+        <button @click="searchKeyword(searchQuery)">
+            Search
+        </button>
+    </div>
+
+    <!-- category buttons -->
+    <div>
+        <p>Filter by category</p>
+        <button
+            v-for = 'cat in CATEGORIES'
+            :key = 'cat'
+        >
+            {{cat}}
+        </button>
+    </div>
 
 </template>
+
+<script setup>
+
+    import {ref, onMounted} from 'vue'
+
+    const CATEGORIES = ref([])
+
+    const searchQuery = ref('')
+
+    function searchKeyword(q) {
+    console.log("Search requested for:", q)
+    }
+
+    onMounted(async () => {
+        try {
+            const res = await fetch('http://127.0.0.1:5000/api/categories')
+            CATEGORIES.value = await res.json()
+        } catch (err) {
+            console.error('Failed to fetch categories:', err)
+        }
+    })
+
+</script>
