87c55c7257b51529884c8aae8aded17cfafff3c6
Gabrielle Edwards <175048781+gabredw@users.noreply.github.com>
2026-02-09 06:39:55 -0800

    Remove unnecessary/mistake line

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import OrgSearch from '../components/OrgSearch.vue'
 import ResultsPage from '../components/ResultsPage.vue'
-import { Component } from 'react'
 
 const routes = [
     {
